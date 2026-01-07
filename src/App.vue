34873e3ee5ec75f08486262fff93ae3dcc1b2882
Ioannis Psychogyios <psic_aek@hotmail.com>
2026-01-07 14:05:32 +0100

    fix minor bug in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,43 @@
 <template>
   <v-app>
-    <!-- Blue gradient app bar matching your form page -->
-    <v-app-bar
-      color="primary"
-      dark
-      elevation="0"
-      "
-      style="
-        background: linear-gradient(
-          135deg,
-          #00b4db, 0%,
-          #001f3f 100%
-        ) !important;
-      "
-    >
-      <v-app-bar-nav-icon><v-img src="src/assets/mainLogo.svg" width="30" height="30" class="mr-2"></v-img></v-app-bar-nav-icon>
+    <v-layout>
+      <!-- Blue gradient app bar matching your form page -->
+      <v-app-bar
+        color="primary"
+        dark
+        elevation="0"
+        style="
+          background: linear-gradient(
+            135deg,
+            #00b4db,
+            0%,
+            #001f3f 100%
+          ) !important;
+        "
+      >
+        <v-app-bar-nav-icon
+          ><v-img
+            src="src/assets/mainLogo.svg"
+            width="30"
+            height="30"
+            class="mr-2"
+          ></v-img
+        ></v-app-bar-nav-icon>
 
-    <div class="d-flex align-center">
-  
-  <span class="font-weight-bold text-h6">RunPlan.dev</span>
-</div>
+        <div class="d-flex align-center">
+          <span class="font-weight-bold text-h6">RunPlan.dev</span>
+        </div>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <v-btn to="/generate" text class="mr-4"> Generator </v-btn>
-      <v-btn to="/about" text> About </v-btn>
-    </v-app-bar>
+        <v-btn to="/generate" text class="mr-4"> Generator </v-btn>
+        <v-btn to="/about" text> About </v-btn>
+      </v-app-bar>
 
-    <v-main>
-      <router-view></router-view>
-    </v-main>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-layout>
   </v-app>
 </template>
 
